fd2e184ed9123fc9132c1b7f6b1dd1c5c7bfbe5a
zhangsgithub04 <ssen_zhang@hotmail.com>
2023-10-17 18:46:03 -0400

    Update test10.py

--- test10.py
+++ test10.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
-
 from ev3dev2.motor import LargeMotor
 from ev3dev2.sensor.lego import ColorSensor
-
 cs = ColorSensor()
 lm = LargeMotor()
-
-lm.on(30)
-while not cs.color == cs.COLOR_YELLOW:
-    pass
+lm.on(10)
 lm.off()
+
+print(cs.color)
+
+if cs.color == cs.COLOR_BLACK:
+    print("BLACK")
+else:
+    print("NOT black")
